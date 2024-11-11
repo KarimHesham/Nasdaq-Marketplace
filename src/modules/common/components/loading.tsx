508f8bc4cd41b324661533fe6e8f8361cac2fa0e
Kareem Hesham <karimdeveloper@outlook.com>
2024-11-11 16:48:39 +0200

    feat(splash-screen): add splash screen

---
+++ src/modules/common/components/loading.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {ActivityIndicator} from 'react-native';
+
+const Loading: React.FC = () => {
+  return <ActivityIndicator size="large" color="#3E3E5E" />;
+};
+
+export default Loading;
--- src/modules/stocks/styles/search-input.styles.ts
+++ src/modules/stocks/styles/search-input.styles.ts
@@ -3,10 +3,18 @@ import {StyleSheet} from 'react-native';
 export const searchInputStyles = StyleSheet.create({
   container: {
     width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
     paddingHorizontal: 16,
     marginBottom: 16,
   },
+  logo: {
+    width: 24,
+    height: 24,
+  },
   input: {
+    width: '85%',
     height: 48,
     backgroundColor: '#2C2C3E',
     borderRadius: 12,
